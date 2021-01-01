1a102a5d692cb0424b8606501842fea40ec81b90
Abdelhadi Khiati <kiloreux@gmail.com>
2021-01-01 10:10:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+
+    const inputs = {
+      token: core.getInput('repo-token', {required: true}),
+    }
+
+    const request = {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: github.context.payload.pull_request.number,
+    }
+
+    const title = github.context.payload.pull_request.title || '';
+    const labels = github.context.payload.pull_request.labels;
+
+    let newTitle;
+    let updateTitle = false;
+
+
+    if (labels.some(e => e.name === 'review')) {
+      newTitle = title.replace('WIP:', '')
+      updateTitle = true
+    } 
+
+    if (!labels.some(e => e.name === 'review') && !title.includes('WIP')) {
+      newTitle = `WIP: ${title}`
+      updateTitle = true
+    } 
+
+    core.setOutput('titleUpdated', newTitle.toString());
+
+    if (updateTitle) {
+      request.title = newTitle;
+      core.info(`New title is: ${request.title}`);
+    } else {
+      core.warning('No need to update PR title');
+    }
+
+
+    if (!updateTitle) {
+      return;
+    }
+
+    const octokit = github.getOctokit(inputs.token);
+    const response = await octokit.pulls.update(request);
+
+    core.info(`Response: ${response.status}`);
+    if (response.status !== 200) {
+      core.error('Updating the pull request has failed');
+    }
+  }
+  catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run()
